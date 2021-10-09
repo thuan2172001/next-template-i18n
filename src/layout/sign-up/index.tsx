a72ee0dc8669fa220a35b6d0e21d9080f6bc374c
nggocnn <nhungocnnn2509@gmail.com>
2021-10-10 00:17:47 +0700

    feat: sigup + formik + yup waiting for api

--- src/layout/sign-up/index.tsx
+++ src/layout/sign-up/index.tsx
@@ -3,179 +3,143 @@ import {Button, Input, Checkbox} from 'antd'
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import { useTranslation } from 'next-i18next'
 import style from './sign-up.module.scss'
-import serie from "../../api/customer/serie";
+import serie from "../../api/customer/serie"
+import { useFormik } from 'formik'
+import * as Yup from "yup"
 
 const SignupTemplate = (props) => {
-
-    const { t } = useTranslation()
-    const [userName, setUserName] = useState("")
-    const [isUserNameType, setIsUserNameType] = useState(false)
-    const [isUserNameExist, setIsUserNameExist] = useState(false)
-
-    const [email, setEmail] = useState("")
-    const [isEmailValid, setIsEmailValid] = useState(false)
-    const [isEmailExist, setIsEmailExist] = useState(false)
-
-    const [fullName, setFullName] = useState("")
-    const [newPassword, setNewPassword] = useState("")
-    const [isNewPasswordTyped, setIsNewPasswordTyped] = useState(false)
-    const [isPasswordValid,setIsPasswordValid] = useState(false)
-    const [confirmPassword, setConfirmPassword] = useState("")
-    const [isConfirmPasswordTyped, setIsConfirmPasswordTyped] = useState(false)
-
-    const [checkboxState, setCheckboxState] = useState(false)
-
-    const handleSignup = () => {
-
-    }
-
-    const handleChangeUserName = (value) => {
-        setUserName(value)
-
-    }
-
-    const handleChangeEmail = (value) => {
-        setEmail(value)
-        emailValidation(value)
-    }
-
-    const emailValidation = (value) => {
-        setIsEmailValid(
-            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@([\w-]+\.)+[\w-]{2,4}$/.test(value)
-        )
-    }
-
-    const handleChangeNewPassword = (value) => {
-        setNewPassword(value)
-        setIsNewPasswordTyped(true)
-        validatePassword(value)
-    }
-
-    const validatePassword = (value) => {
-        value.length < 8 ||
-        !RegExp(".*[A-Z].*").test(value) ||
-        !RegExp(".*[a-z].*").test(value) ||
-        !RegExp(".*\\d.*").test(value) ||
-        value != value.normalize("NFD").replace(/[\u0300-\u036f]/g, "") ||
-        /\s/.test(value)
-            ? setIsPasswordValid(false)
-            : setIsPasswordValid(true);
-    }
-
-    const handleChangeConfirmPassword = (value) => {
-        setIsConfirmPasswordTyped(true);
-        setConfirmPassword(value);
-    };
+    const formik = useFormik({
+        initialValues: {
+            user_name: "",
+            email: "",
+            full_name: "",
+            password: "",
+            confirm_password: "",
+            checkbox: false,
+        },
+        validationSchema: Yup.object({
+            user_name: Yup.string()
+                .min(6, "Minimum 6 characters")
+                .max(10, "Maximum 15 characters")
+                .required("Required!"),
+            email: Yup.string()
+                .email("Invalid email format")
+                .required("Required!"),
+            full_name: Yup.string()
+                .required("Required!"),
+            password: Yup.string()
+                .matches(
+                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\()_+=\-/*+{}|\[\]'"<,>.?/~`\\])(?=.{8,})/,
+                    "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+                )
+                .required("Required!"),
+            confirm_password: Yup.string()
+                .oneOf([Yup.ref("password")], "Password's not match")
+                .required("Required!"),
+            checkbox: Yup.boolean().oneOf([true], "Please check the box!")
+        }),
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        }
+    })
 
     return (
         <div className={style["container"]}>
             <div className={style["signup-container"]}>
                 <div className={style['signup-title']}>Create new account</div>
-                <div className={style['signup-form']}>
+                <form onSubmit={formik.handleSubmit} className={style['signup-form']}>
                     <h4>User Name</h4>
                     <Input
                         className={`${style['ant-input-custom']} ${style['ant-input-signup-form']}`}
                         placeholder={"User Name"}
+                        name="user_name"
                         autoComplete="off"
-                        onChange={(e) => {
-                            setUserName(e.target.value)
-                        }}
+                        value={formik.values.user_name}
+                        onChange={formik.handleChange}
                     />
+
+                    {formik.errors.user_name && formik.touched.user_name && (
+                        <p className={`${style["signup-notify"]}`}>{formik.errors.user_name}</p>
+                    )}
 
                     <h4>Email address</h4>
                     <Input
                         className={`${style["ant-input-custom"]} ${style['ant-input-signup-form']}`}
                         placeholder="Your email address"
+                        name="email"
                         autoComplete="off"
-                        onChange={(e) => {
-                            handleChangeEmail(e.target.value);
-                        }}
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
                     />
+
+                    {formik.errors.email && formik.touched.email && (
+                        <p className={`${style["signup-notify"]}`}>{formik.errors.email}</p>
+                    )}
 
                     <h4>Full name</h4>
                     <Input
                         className={`${style["ant-input-custom"]} ${style['ant-input-signup-form']}`}
                         placeholder="Your full name"
+                        name="full_name"
                         autoComplete="off"
-                        onChange={(e) => {
-                            setFullName(e.target.value)
-                        }}
+                        value={formik.values.full_name}
+                        onChange={formik.handleChange}
                     />
+
+                    {formik.errors.full_name && formik.touched.full_name && (
+                        <p className={`${style["signup-notify"]}`}>{formik.errors.full_name}</p>
+                    )}
 
                     <h4>Password</h4>
                     <Input.Password
                         className={`${style['ant-input-custom']} ${style['ant-input-signup-form']}`}
                         placeholder="Your password"
+                        name="password"
                         autoComplete="off"
-                        type="password"
                         iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-                        onChange={(e) => {
-                            handleChangeNewPassword(e.target.value)
-                        }}
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
                     />
 
-                    {isNewPasswordTyped &&
-                        (newPassword == "" ? (
-                                <div className={`${style["signup-notify"]}`}>
-                                    Please input your password
-                                </div>
-                            ) : isPasswordValid ? (
-                                <></>
-                            ) : (
-                                <div className={`${style["signup-notify"]}`}>
-                                    {t("account:newPasswordSyntax")}
-                                </div>
-                            )
-                        )
-                    }
+                    {formik.errors.password && formik.touched.password && (
+                        <p className={`${style["signup-notify"]}`}>{formik.errors.password}</p>
+                    )}
 
                     <h4>Confirm your password</h4>
                     <Input.Password
                         className={`${style['ant-input-custom']} ${style['ant-input-signup-form']}`}
                         placeholder="Confirm your password"
+                        name="confirm_password"
                         autoComplete="off"
-                        type="password"
                         iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-                        onChange={(e) => {
-                            handleChangeConfirmPassword(e.target.value)
-                        }}
+                        value={formik.values.confirm_password}
+                        onChange={formik.handleChange}
                     />
 
-                    {isConfirmPasswordTyped &&
-                        (confirmPassword != "" ? (
-                            confirmPassword == newPassword ? (
-                                <></>
-                            ) : (
-                                <div className={`${style["signup-notify"]} ${style["text-color-red"]}`}>
-                                    {t("account:confirmPasswordNotMatch")}
-                                </div>
-                            )
-                            ) : (
-                                <div className={`${style["signup-notify"]} ${style["text-color-red"]}`}>
-                                    Please confirm your password
-                                </div>
-                            )
-                        )
-                    }
+                    {formik.errors.confirm_password && formik.touched.confirm_password && (
+                        <p className={`${style["signup-notify"]}`}>{formik.errors.confirm_password}</p>
+                    )}
 
                     <Checkbox
-                        defaultChecked={false}
-                        onChange={(e) => {
-                            setCheckboxState(!checkboxState)
-                        }}
+                        name="checkbox"
+                        onChange={formik.handleChange}
                     >
                         I have read and agree to the <a href="">Terms of Uses</a> and <a href="">Privacy Policy</a>
                     </Checkbox>
-                </div>
 
-                <div className={style['btn-controller']}>
-                    <Button
-                        className={`${style['ant-btn-signup']}`}
-                        onClick={handleSignup}
-                    >
-                        {t('common:header.signUp')}
-                    </Button>
-                </div>
+                    {formik.errors.checkbox && formik.touched.confirm_password && (
+                        <p className={`${style["signup-notify"]}`}>{formik.errors.checkbox}</p>
+                    )}
+
+                    <div className={style['btn-controller']}>
+                        <Button
+                            htmlType="submit"
+                            className={`${style['ant-btn-signup']}`}
+                        >
+                            Sign up
+                        </Button>
+                    </div>
+                </form>
             </div>
         </div>
     );
