7565f22159049944cc522b4c330195fffcfc8901
nggocnn <nhungocnnn2509@gmail.com>
2021-11-03 23:37:14 +0700

    feat: in-page-load-effect complete

--- src/components/load-effect/index.tsx
+++ src/components/load-effect/index.tsx
@@ -4,10 +4,15 @@ import LoaderGif from '../../../public/assets/images/loader.gif';
 import style from './load-effect.module.scss';
 export const LoadEffect = () => {
   return (
-    <div className="loader-container">
-      <div className="loader">
-        <Image src={LoaderGif} />
-      </div>
+    <div className={`${style["bookshelf_wrapper"]}`}>
+      <ul className="books_list">
+        <li className={`${style["book_item"]} ${style["first"]}`}></li>
+        <li className={`${style["book_item"]} ${style["second"]}`}></li>
+        <li className={`${style["book_item"]} ${style["third"]}`}></li>
+        <li className={`${style["book_item"]} ${style["fourth"]}`}></li>
+        <li className={`${style["book_item"]} ${style["fifth"]}`}></li>
+        <li className={`${style["book_item"]} ${style["sixth"]}`}></li>
+      </ul>
     </div>
   );
 }
