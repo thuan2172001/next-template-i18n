8ed21e1ad884fc7718c1b645f38105bf40ba878a
nggocnn <nhungocnnn2509@gmail.com>
2021-11-20 00:26:26 +0700

    fix add title

--- src/components/load-effect/index.tsx
+++ src/components/load-effect/index.tsx
@@ -6,12 +6,12 @@ export const LoadEffect = () => {
   return (
     <div className={`${style["bookshelf_wrapper"]}`}>
       <ul className="books_list">
-        <li className={`${style["book_item"]} ${style["first"]}`}></li>
-        <li className={`${style["book_item"]} ${style["second"]}`}></li>
-        <li className={`${style["book_item"]} ${style["third"]}`}></li>
-        <li className={`${style["book_item"]} ${style["fourth"]}`}></li>
-        <li className={`${style["book_item"]} ${style["fifth"]}`}></li>
-        <li className={`${style["book_item"]} ${style["sixth"]}`}></li>
+        <li className={`${style["book_item"]} ${style["first"]}`}/>
+        <li className={`${style["book_item"]} ${style["second"]}`}/>
+        <li className={`${style["book_item"]} ${style["third"]}`}/>
+        <li className={`${style["book_item"]} ${style["fourth"]}`}/>
+        <li className={`${style["book_item"]} ${style["fifth"]}`}/>
+        <li className={`${style["book_item"]} ${style["sixth"]}`}/>
       </ul>
     </div>
   );
